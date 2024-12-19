4a859a4fda44fac8af46160e0774f813536219cd
harshal-dev1 <harshal-dev1@demo.lab>
2024-12-19 11:50:38 +0530

    added substract functionality

--- addition.py
+++ addition.py
@@ -11,6 +11,8 @@ n1 = int(input("Enter the first number : "))
 n2 = int(input("Enter the second number : "))
 
 sum = n1+n2
+sub = n1-n2
 mul = n1*n2
 print(f"The sum of {n1} and {n2} is {sum}")
 print(f"The multiplication of {n1} and {n2} is {mul}")
+print(f"The substraction of {n1} and {n2} is {sub}")
